1fa51314bb47f968203ae8dbf2dded608762955f
Jitimay <jitimayjosh1@gmail.com>
2026-02-25 02:15:26 +0200

    resolving finnished

--- imbuto/lib/shared/services/service_locator.dart
+++ imbuto/lib/shared/services/service_locator.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:imbuto/core/services/api_service.dart';
 import '../../core/network/api_client.dart';
 import '../../features/auth/data/datasources/auth_remote_datasource.dart';
 import '../../features/auth/data/repositories/auth_repository_impl.dart';
@@ -14,27 +15,27 @@ class ServiceLocator {
     // Core
     sl.registerLazySingleton(() => ApiClient());
     sl.registerLazySingleton(() => ApiService(sl()));
-    
+
     // Data sources
     sl.registerLazySingleton<AuthRemoteDataSource>(
-      () => AuthRemoteDataSourceImpl(sl()),
+      () => AuthRemoteDataSource(apiClient: sl()),
     );
-    
+
     // Repositories
     sl.registerLazySingleton<AuthRepository>(
       () => AuthRepositoryImpl(apiClient: sl()),
     );
-    
+
     // Use cases
     sl.registerLazySingleton(() => LoginUseCase(repository: sl()));
     sl.registerLazySingleton(() => RegisterUseCase(repository: sl()));
-    
+
     // BLoCs
     sl.registerFactory(() => AuthBloc(
-      loginUseCase: sl(),
-      registerUseCase: sl(),
-    ));
+          loginUseCase: sl(),
+          registerUseCase: sl(),
+        ));
   }
-  
+
   static T get<T extends Object>() => sl<T>();
 }
